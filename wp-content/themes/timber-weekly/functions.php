ae747de03261f412463fe8197d67d3315c593c4e
Alexander Gross <agross@bangordailynews.com>
2016-05-13 14:21:20 -0400

    Base Template near complete for mobile

--- wp-content/themes/timber-weekly/functions.php
+++ wp-content/themes/timber-weekly/functions.php
@@ -19,8 +19,11 @@ class StarterSite extends \Timber\Site {
 		add_theme_support( 'menus' );
 		add_filter( 'timber_context', array( $this, 'add_to_context' ) );
 		add_filter( 'get_twig', array( $this, 'add_to_twig' ) );
+		add_filter('show_admin_bar', '__return_false');
 		add_action( 'init', array( $this, 'register_post_types' ) );
 		add_action( 'init', array( $this, 'register_taxonomies' ) );
+		add_action( 'wp_enqueue_scripts', array( $this, 'add_scripts' ) );
+
 		parent::__construct();
 	}
 
@@ -51,6 +54,12 @@ class StarterSite extends \Timber\Site {
 		$twig->addExtension( new Twig_Extension_StringLoader() );
 		$twig->addFilter('myfoo', new Twig_SimpleFilter('myfoo', array($this, 'myfoo')));
 		return $twig;
+	}
+
+	function add_scripts() {
+		//wp_enqueue_style( 'weekly-style', get_stylesheet_uri() );
+		wp_enqueue_script( 'jquery' );
+		wp_enqueue_script( 'site-default', get_template_directory_uri() . '/static/site.js', array(), '1', true);
 	}
 
 }
--- wp-content/themes/timber-weekly/static/site.js
+++ wp-content/themes/timber-weekly/static/site.js
@@ -1,5 +1,31 @@
 jQuery( document ).ready( function( $ ) {
 
-  // Your JavaScript goes here
+    // Toggle Search
+    console.log("Initialize Search Bar");
+    var navSearchIcon = document.getElementById("search-icon");
+    var navSearchBar  = document.getElementById("search-wrapper");
+    navSearchIcon.addEventListener("click", function() {
+        navSearchBar.classList.toggle("search-down");
+        console.log("toggle: Search Bar");
+    });
 
+    // Toggle Search
+    console.log("Initialize Side Menu");
+    var menuButton = document.getElementById('m-button');
+    var overlayButton = document.getElementById('content-overlay');
+    //var buttons = [menuButton,overlayButton];
+
+    /**
+     * Select all Menu Targets by ClassName and convert the HTMLCollection to an Array
+     * @var array menuTargets Array of Menu Target to activate onclick
+     */
+    var menuTargets = [].slice.call( document.getElementsByClassName('m-target') );
+    [ menuButton, overlayButton ].forEach( function( button ) {
+        button.addEventListener("click", function() {
+            menuTargets.forEach( function( target ) {
+                target.classList.toggle('is-active')
+            });
+            console.log("toggle: Side Menu")
+        });
+    });
 });
